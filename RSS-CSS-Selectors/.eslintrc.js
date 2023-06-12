ab3287728a3a7b6e15e944c6f3ef56bb19c0e8ea
Glitch-surfer <marker661@gmail.com>
2023-06-12 12:14:10 +0300

    feat: add eslint, typescript, webpack

---
+++ RSS-CSS-Selectors/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-inferrable-types': 'off',
+  },
+  root: true
+}
---
+++ RSS-CSS-Selectors/readme.md
@@ -0,0 +1,6 @@
+#RSS-CSS-Selectors
+---
+
+*Game, where you can learn css selectors with fun!
+---
+*Enjoy😎
