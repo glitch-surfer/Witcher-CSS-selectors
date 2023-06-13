548daa5580d64838e7f16884253f9d54e8c4cb98
Glitch-surfer <marker661@gmail.com>
2023-06-13 15:53:14 +0300

    fix: fix error on empty input

--- RSS-CSS-Selectors/src/components/game/remove-element.ts
+++ RSS-CSS-Selectors/src/components/game/remove-element.ts
@@ -4,10 +4,11 @@ const removeElement = (): void => {
   const input = app.main.getHtmlElement().children[0].children[0].children[2] as HTMLInputElement;
   const selector = input.value;
   const table = app.header.getHtmlElement().lastElementChild;
-
-  table?.querySelectorAll(selector)?.forEach((element) => {
-    element.remove();
-  });
+  if (selector !== '') {
+    table?.querySelectorAll(selector)?.forEach((element) => {
+      element.remove();
+    });
+  }
 };
 
 export { removeElement };
