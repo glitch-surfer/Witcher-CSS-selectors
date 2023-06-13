f65a332aa4eac0bbefb216ff92d728e946688a06
Glitch-surfer <marker661@gmail.com>
2023-06-13 11:16:43 +0300

    feat: add aside component

---
+++ RSS-CSS-Selectors/src/components/aside/aside-view.ts
@@ -0,0 +1,42 @@
+import type { IParams } from '../../types/types';
+import { View } from '../util/view';
+
+class AsideView extends View {
+  constructor() {
+    const asideParams: IParams = {
+      tag: 'aside',
+      className: ['levels'],
+      children: [
+        {
+          tag: 'h3',
+          className: ['levels__title'],
+          text: 'Level',
+        },
+        {
+          tag: 'ul',
+          className: ['levels__list'],
+          children: [
+            {
+              tag: 'li',
+              className: ['levels__item'],
+              text: '1',
+            },
+            {
+              tag: 'li',
+              className: ['levels__item'],
+              text: '2',
+            },
+            {
+              tag: 'li',
+              className: ['levels__item'],
+              text: '3',
+            },
+          ],
+        },
+      ],
+    };
+    super(asideParams);
+  }
+}
+
+export { AsideView };
