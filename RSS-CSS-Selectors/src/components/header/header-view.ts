a402127574e18e77c6414aa47eced515e6963c54
Glitch-surfer <marker661@gmail.com>
2023-06-13 11:03:16 +0300

    feat: add header base layout

--- RSS-CSS-Selectors/src/components/header/header-view.ts
+++ RSS-CSS-Selectors/src/components/header/header-view.ts
@@ -1,15 +1,23 @@
 import './header.scss';
 import { View } from '../util/view';
 import type { IParams } from '../../types/types';
-// import { ElementGenerator } from '../util/element-generator';
 
 class HeaderView extends View {
   constructor() {
     const HeaderParams: IParams = {
       tag: 'header',
       className: ['header'],
-      text: 'Header',
-      // callback:
+      children: [
+        {
+          tag: 'h1',
+          className: ['header__title'],
+          text: 'Witcher`s Adventures',
+        },
+        {
+          tag: 'div',
+          className: ['header__table'],
+        },
+      ],
     };
     super(HeaderParams);
   }
