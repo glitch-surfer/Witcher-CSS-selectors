b2c8205b31f7e3e4cace238e07562d690e8e708c
Glitch-surfer <marker661@gmail.com>
2023-06-15 22:26:57 +0300

    refactor: delete useless coments

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,11 +1,24 @@
+// import { EditorState } from '@codemirror/state';
+// import { EditorView, keymap } from '@codemirror/view';
+// import { defaultKeymap } from '@codemirror/commands';
+
 import { App } from './components/app';
 
-const app = new App();
+// const startState = EditorState.create({
+//   doc: 'Hello World',
+//   extensions: [keymap.of(defaultKeymap)],
+// });
+
+// const view = new EditorView({
+//   state: startState,
+//   parent: document.body,
+// });
+
+export const app = new App();
 app.createView();
 
-// if (!(app.footer.viewElement.getElement().firstElementChild?.textContent)) {
-//   app.footer.viewElement.getElement().firstElementChild?.textContent = '';
-// }
+// hljs.highlightAll();
 
-// console.log(app.footer.viewElement.getElement().firstElementChild?.textContent);
-export { app };
+// app.addActiveClass();
+// const datatest = app.table?.firstElementChild as HTMLElement;
+// console.log(datatest?.dataset);
