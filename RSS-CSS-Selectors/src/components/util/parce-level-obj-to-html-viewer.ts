d3538452200ac8b2394fcf7f2a3817576cad8ef5
Glitch-surfer <marker661@gmail.com>
2023-06-16 09:47:06 +0300

    refactor: correct naming, delete consolelog

--- RSS-CSS-Selectors/src/components/util/parce-level-obj-to-html-viewer.ts
+++ RSS-CSS-Selectors/src/components/util/parce-level-obj-to-html-viewer.ts
@@ -1,11 +1,10 @@
-// lib
 import 'highlight.js/styles/github.css';
 import hljs from 'highlight.js/lib/core';
 import xml from 'highlight.js/lib/languages/xml';
 import type { IParams } from '../../types/types';
 
 hljs.registerLanguage('xml', xml);
-export const parceLevelObjToHtmlViewer = (level: IParams): HTMLElement => {
+export const parseLevelObjToHtmlViewer = (level: IParams): HTMLElement => {
   const result = document.createElement('div');
   let start;
   let end;
@@ -24,23 +23,22 @@ export const parceLevelObjToHtmlViewer = (level: IParams): HTMLElement => {
     [start, end] = [`<${level.tag}>`, `</${level.tag}>`];
   }
 
-  const coloredStartHtmlAsString = hljs.highlight(start, { language: 'xml' }).value;
   const startElement = document.createElement('span');
-  startElement.innerHTML = coloredStartHtmlAsString;
-
   result.append(startElement);
+  const coloredStartHtmlAsString = hljs.highlight(start, { language: 'xml' }).value;
+  startElement.outerHTML = coloredStartHtmlAsString;
 
   if (level.children !== undefined) {
     level.children.forEach((child) => {
-      const children = parceLevelObjToHtmlViewer(child);
+      const children = parseLevelObjToHtmlViewer(child);
       result.append(children);
     });
   }
 
-  const coloredEndHtmlAsString = hljs.highlight(end, { language: 'xml' }).value;
   const endElement = document.createElement('span');
-  endElement.innerHTML = coloredEndHtmlAsString;
-
   result.append(endElement);
+  const coloredEndHtmlAsString = hljs.highlight(end, { language: 'xml' }).value;
+  endElement.outerHTML = coloredEndHtmlAsString;
+
   return result;
 };
--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -2,4 +2,3 @@ import { App } from './components/app';
 
 export const app = new App();
 app.createView();
-console.log(app.table)
