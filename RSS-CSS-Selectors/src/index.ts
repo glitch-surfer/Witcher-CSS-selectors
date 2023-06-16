a4b5b206c063205439ba26efa24a9af0545ac78f
Glitch-surfer <marker661@gmail.com>
2023-06-16 08:06:16 +0300

    refactor: delete comments

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,24 +1,5 @@
-// import { EditorState } from '@codemirror/state';
-// import { EditorView, keymap } from '@codemirror/view';
-// import { defaultKeymap } from '@codemirror/commands';
-
 import { App } from './components/app';
-
-// const startState = EditorState.create({
-//   doc: 'Hello World',
-//   extensions: [keymap.of(defaultKeymap)],
-// });
-
-// const view = new EditorView({
-//   state: startState,
-//   parent: document.body,
-// });
 
 export const app = new App();
 app.createView();
-
-// hljs.highlightAll();
-
-// app.addActiveClass();
-// const datatest = app.table?.firstElementChild as HTMLElement;
-// console.log(datatest?.dataset);
+console.log(app.table)
