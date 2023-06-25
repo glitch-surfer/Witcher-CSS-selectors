4c6ae5ef6465911ce953f957a3cf86eb05e107a0
Glitch-surfer <marker661@gmail.com>
2023-06-25 16:33:20 +0300

    feat: implement 4 lvl

--- RSS-CSS-Selectors/src/components/game/levels/level-4/level-4.ts
+++ RSS-CSS-Selectors/src/components/game/levels/level-4/level-4.ts
@@ -1,10 +1,11 @@
 import type { ILevelParams } from '../../../../types/types';
+import './level-4.scss';
 
 export const level4: ILevelParams = {
   id: 4,
   task: 'Grab the horseshoes!',
   story: 'Geralt did not forget about his favorite horse, Roach, and decided to buy new horseshoes for her with the remaining money from the last order. Who knows when the next opportunity will be. Witchers have less and less work lately... He liked those new ones in the box!',
-  help: ' bento',
+  help: 'box horseshoe',
   modal: {
     tag: 'div',
     className: ['modal'],
@@ -28,17 +29,33 @@ export const level4: ILevelParams = {
   },
   layout: [
     {
-      tag: 'plate',
-      className: ['level1'],
+      tag: 'horseshoe',
       attributes: {
         'data-id': '1',
       },
+    },
+    {
+      tag: 'horseshoe',
+      attributes: {
+        'data-id': '2',
+      },
+    },
+    {
+      tag: 'horseshoe',
+      attributes: {
+        'data-id': '3',
+      },
+    },
+    {
+      tag: 'box',
+      attributes: {
+        'data-id': '4',
+      },
       children: [
         {
-          tag: 'bento',
-          className: ['level1_1'],
+          tag: 'horseshoe',
           attributes: {
-            'data-id': '2',
+            'data-id': '5',
             'data-target': 'true',
           },
         },
