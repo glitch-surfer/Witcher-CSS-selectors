149948ef0f0401191fcccd78fbcd12ea93b9f708
Glitch-surfer <marker661@gmail.com>
2023-07-02 18:16:12 +0300

    fix: correct animation in 11 lvl

--- RSS-CSS-Selectors/src/components/modal/modal-view.ts
+++ RSS-CSS-Selectors/src/components/modal/modal-view.ts
@@ -18,7 +18,7 @@ export const winModalParams: IParams = {
     {
       tag: 'P',
       className: ['modal__text'],
-      text: 'only stronges can go through this game',
+      text: 'Only stronges can go through this game. I hope you enjoyed the game! You have practiced with CSS selectors. Perhaps you even became interested in the Witcher universe, if you were not familiar with it before. If you have any ideas about what new levels and stories to add: feel free to write to me (my email in github profile), I will complete the game and point out who helped me!',
     },
   ],
 };
